6211109eccc560df051d465eb79184f0e47377e2
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-23 09:38:18 -0600

    added a post route and finished get one route

--- app.js
+++ app.js
@@ -17,7 +17,25 @@ app.get('/cakes/:id', (req, res, next) => {
         next()
     }
     const cake = cakes.filter(item => item.id == id)[0]
-    res.json({cake: cakes})
+    res.json({cake: cake})
 })
+app.post('/', (req, res, next) =>{
+    const body = req.body
+    cakes.push(body)
+    res.json({cakes: cakes})
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+    res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
 
 app.listen(port, () => console.log(`I got you on ${port}`))
