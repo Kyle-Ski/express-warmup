15cb3d6f3033042cbce99da44ee6f34cd01cdc84
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-23 09:31:33 -0600

    adding get all and get one route

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const cakes = require('./data.json')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => {
+    res.json({cakes: cakes})
+})
+
+app.get('/cakes/:id', (req, res, next) => {
+    const id = req.params.id
+    if (!(Number(id))){
+        next()
+    }
+    const cake = cakes.filter(item => item.id == id)[0]
+    res.json({cake: cakes})
+})
 
 app.listen(port, () => console.log(`I got you on ${port}`))
